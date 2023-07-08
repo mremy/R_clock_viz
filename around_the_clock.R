a770e24687b69bc9ea9e3391bf8ba02361bacb46
Marc Remy <mremy@users.noreply.github.com>
2023-07-08 16:07:36 +0200

    Reword comments, remove French

--- around_the_clock.R
+++ around_the_clock.R
@@ -2,25 +2,31 @@ library(dplyr, warn.conflicts = FALSE)
 library(ggplot2)
 library(viridis)
 
+# Start again, with this example:
+# https://r-graph-gallery.com/circular-barplot.html
+# https://r-graph-gallery.com/web-circular-lollipop-plot-with-ggplot2.html
+# (but the scale is the same for each categories).
+#
 # The data
 hdc <- data.frame(read.delim("re4b_commits.csv", sep=";", header = TRUE))
 
-# Nous filtrons ici les auteurs avec au moins 48 commits, et cherchons
-# comment ils sont répartis suivant l'heure.
-# p contient le maximum pour un utilisateur, afin d'avoir une échelle
-# pour chacun.
-#
-# We keep only authors with 48+ commits, and search how they split
-# during the day.
-# p hold the maximum for each user, to have a separate scale.
-#
-hdc2 <-hdc %>% group_by(Author) %>%
-  mutate(nb=n()) %>%
-  filter(nb>=48) %>%
-  group_by(Author,Hour) %>%
-  mutate(c=n()) %>%
-  group_by(Author) %>%
-  mutate(p=max(c))
+# Keep only the columns we need: 'Author' and 'Hour'
+hdc <- hdc %>% select(Author, Hour)
+
+# Count the commits per Author and Hour,
+# keep the number per Hour and the total per Author,
+# as we want a per Author scale, to keep it readable
+# for Author having a smaller number of commits
+# (between 48, our minimum choosen value and
+# more than 2k for the author of the book).
+hdc <-hdc %>% group_by( Author ) %>%
+  mutate( nb=n() ) %>%
+  filter( nb>=48 ) %>%
+  group_by( Author, Hour ) %>%
+  mutate( c=n() ) %>%
+  group_by( Author ) %>%
+  mutate(p=max(c)) %>%
+  distinct()
 
 update_geom_defaults("text", list(colour = "black", alpha=0.3, family = theme_get()$text$family))
 
@@ -41,21 +47,22 @@ our_labs_title <- our_labs_title_en
 # to have 100% for each user. The absolute number is based
 # on the color.
 #
-g11 <- hdc2 %>%
+g11 <- hdc %>%
   ungroup() %>%
-  group_by(Author) %>% ggplot( aes(x=Hour ) ) +
-  geom_rect( aes( fill=c, xmin=Hour, xmax=Hour+1, ymin=0, ymax=100*c/p ) ) +
-  scale_x_continuous( breaks=0:23 ) +
-  labs( title=our_labs_title, x=NULL, y=NULL ) +
-  theme_bw() +
-  theme(axis.ticks.y = element_blank(),
+  group_by(Author) %>%
+  ggplot( aes(x=Hour ) ) +
+    geom_rect( aes( fill=c, xmin=Hour, xmax=Hour+1, ymin=0, ymax=100*c/p ) ) +
+    scale_x_continuous( breaks=0:23 ) +
+    labs( title=our_labs_title, x=NULL, y=NULL ) +
+    theme_bw() +
+    theme(axis.ticks.y = element_blank(),
         axis.text.y  = element_blank(),
         panel.border = element_blank()
         ) +
-  scale_fill_distiller( palette = our_palette, name = our_labs_y ) +
-  coord_polar(start=0) +
-  facet_wrap(. ~ Author, ncol=4) +
-    geom_text( aes(label=c(nb), x=0, y=-50 ))
+    scale_fill_distiller( palette = our_palette, name = our_labs_y ) +
+    coord_polar(start=0) +
+    facet_wrap(. ~ Author, ncol=4) +
+    geom_text( aes(label=paste0(nb), x=0, y=-50 ) )
 
 # Save into a PNG file
 #
